12d0b46a43f0959acb96be6d2f42aa0709068478
CozmoZK-Silvestre <silvestregabriel2019@gmail.com>
2025-10-21 10:20:45 +0000

    Retoques finais do exercicio de Herança

--- ListaHerança/src/Aplicativo5.java
+++ ListaHerança/src/Aplicativo5.java
@@ -2,6 +2,13 @@ import java.time.LocalDate;
 public class Aplicativo5 {
     public static void main(String[] args) {
         Ingresso i1=new Ingresso(50, "Cascomadeira", LocalDate.of(2025,10,17));
+        Vip i2=new Vip(500, "Binks no sake",LocalDate.of(2025,02,17), 200);
+        CamaroteSuperior i3=new CamaroteSuperior(500, "Binks no Sake",LocalDate.of(2025,12,03),200,"A2",100);
+        Normal i4=new Normal(1200, "Crossworlds", LocalDate.of(2025,9,04));
         i1.imprimeDadosDoEvento();
+        System.out.println(i2.valorrsVip()); 
+        i3.imprimeLocalizacao();
+        i4.imprimirIngresso();
+
     }
 }
